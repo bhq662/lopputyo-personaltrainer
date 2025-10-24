065be5148b5a9b5bdb96e9d9ddced22d467d4a72
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-10-24 12:09:12 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import Carlist from "./components/Carlist";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+// app bar import
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function App() {
+
+  return (
+    <>
+      <Container maxWidth="lg">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography>Personal Training</Typography>
+          </Toolbar>
+        </AppBar>
+        {/* <Carlist /> */}
+        <CssBaseline />
+      </Container>
+    </>
+  )
+}
