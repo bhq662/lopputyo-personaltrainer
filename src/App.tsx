db8c2af1da9180311fe62c8dd97578496fdfa84d
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-11-14 16:33:31 +0200

    Routing, navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import Customerlist from "./components/Customerlist";
+import { Link, Outlet } from "react-router";
 
 // style imports
+import Button from '@mui/material/Button';
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 
 // app bar import
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Traininglist from "./components/Traininglist";
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function App() {
 
@@ -17,13 +62,37 @@ export default function App() {
       <Container maxWidth="lg">
         <AppBar position="static">
           <Toolbar>
-            <Typography>Personal Training</Typography>
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Personal Training
+            </Typography>
+
+            <Button color="inherit" component={Link} to="/">Customers</Button>
+            <Button color="inherit" component={Link} to="/gettrainings">Trainings</Button>
+
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
           </Toolbar>
         </AppBar>
-        <h3>Customers</h3>
+
+        <Outlet />
+
+        {/* <h3>Customers</h3>
         <Customerlist />
         <h3>Trainings</h3>
-        <Traininglist />
+        <Traininglist /> */}
         <CssBaseline />
       </Container>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import React from "react";
+import App from './App.tsx';
+import ReactDOM from "react-dom/client";
+import Customerlist from './components/Customerlist.tsx';
+import Traininglist from './components/Traininglist.tsx';
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  // import used components
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <Customerlist />,
+        index: true,
+      },
+      {
+        path: "gettrainings",
+        element: <Traininglist />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
